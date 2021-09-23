c5dfaa85124abec8e40b84b72e9553cd00eb0213
Baris Gece <gece.baris@gmail.com>
2021-09-23 19:34:39 +0300

    mutate plugin lowercase option added

--- volumes/logstash/config/queries/incremental.sql
+++ volumes/logstash/config/queries/incremental.sql
@@ -1,4 +1,4 @@
 SELECT *, UNIX_TIMESTAMP(modification_date) AS unix_ts_in_secs 
 FROM teams 
-WHERE (UNIX_TIMESTAMP(modification_date) > :sql_last_value AND modification_date < NOW()) 
+WHERE (UNIX_TIMESTAMP(modification_date) > :sql_last_value AND modification_date < NOW())
 ORDER BY modification_date ASC
